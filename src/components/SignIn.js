5e2f357c47845826c6972b2e1b4ddba83a99eb6f
Karthik Uppala <karthikgupta999@gmail.com>
2019-09-11 00:25:25 +0530

    refs#master LoginPage inital commit

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Typography } from '@material-ui/core';
+
+const useStyles =  makeStyles(theme => ({
+    '@global':{
+         body:{
+             backgroundColor:theme.palette.common.white
+         },
+    },
+     paper: {
+       marginTop: theme.spacing(8),
+       display:'flex',
+       alignItems: 'center',
+       flexDirection:'center',
+     },
+   }));
 
 
 class SignIn extends React.Component{
     render(){
-        return<div>SignIn</div>
+        const classes ={}
+        return(
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <div className ={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        SignIn
+                    </Typography>
+
+                </div>
+            </Container>
+        );
     }
 }
 
